8d07a1b79e7215fc5ed6c5e24037edf0b9922731
Ruthvik Udutha <udutharuthvik@gmail.com>
2019-09-05 02:06:29 -0400

    Add files via upload

---
+++ lib/vampire.ex
@@ -0,0 +1,18 @@
+defmodule Vampire do
+  @moduledoc """
+  Documentation for Vampire.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vampire.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
